9bc4347f81b9d61c9a44c29f6cd508abe0cea754
Anastasia <aachuchkalova@gmail.com>
2023-12-16 23:20:22 +0700

    7.3 Предварительное заполнение SearchView     (PhotoGalleryFragment.kt)     Открытие поисковой фразы из     PhotoGalleryViewModel (PhotoGalleryViewModel.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -1,19 +1,31 @@
 package com.bignerdranch.android.photogallery
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
-class PhotoGalleryViewModel : ViewModel() {
+class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app) {
     val galleryItemLiveData: LiveData<List<GalleryItem>>
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
+    val searchTerm: String
+        get() = mutableSearchTerm.value ?: ""
     init {
-        mutableSearchTerm.value = "planets"
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
         galleryItemLiveData =
-            FlickrFetchr(). searchPhotos("planets")
+            Transformations.switchMap(mutableSearchTerm) { searchTerm ->
+                if (searchTerm.isBlank()) {
+                    flickrFetchr.fetchPhotos()
+                } else {
+                    flickrFetchr.searchPhotos(searchTerm)
+                }
+    }
     }
     fun fetchPhotos(query: String = "") {
+        QueryPreferences.setStoredQuery(app, query)
         mutableSearchTerm.value = query
     }
 }
